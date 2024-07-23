e80f934c3ef042afd55d1490d5932e91d684532f
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2024-07-24 07:23:52 +0800

    committing for preservation

--- drawer.c
+++ drawer.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
-#include <string.h>
 #include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include "graph.h"
+#include "graph.c"
+#include "def.h"
 
 /*
  ! Normal Graph
@@ -14,13 +17,15 @@
  ! Binary Tree
  * Generally, the drawing of the graphs will be in this format
  *
- *               ╔═══════════╗
+ * Note: opted to use = instead of ═ due to overflowing reasons
+ *
+ *               ╔===========╗
  *               ║<Node Data>║
- *               ╚═══════════╝
+ *               ╚===========╝
  *           ┌─────────┴─────────┐
- *     ╔═══════════╗       ╔═══════════╗
+ *     ╔===========╗       ╔===========╗
  *     ║<Node Data>║       ║<Node Data>║
- *     ╚═══════════╝       ╚═══════════╝
+ *     ╚===========╝       ╚===========╝
  *  ┌────────┴────────┐ ┌────────┴────────┐
  *  ╧                 ╧ ╧                 ╧
  *  Notes:
@@ -29,35 +34,51 @@
  *  3.) Width per box is: 2 + <Node Data> length
  *  4.) Minimum spacing between nodes is 7. Excludes null nodes
  *
- *  General algorithm:
- *  1.) Root node: Get spacing required for root node, print root node
  *
  */
-void printBinaryTree()
+
+void printTree(Graph *graph, int rootIndex);
+void printNode(Node *node);
+void repeatPrint(char c, int n);
+
+int main()
+{
+    Graph *graph = createGraph(3);
+    addVertex(graph, "A");
+    addVertex(graph, "B");
+    addVertex(graph, "C");
+    addEdge(graph, "A", "B");
+    addEdge(graph, "A", "C");
+
+    printTree(graph, 0);
+    return 0;
+}
+
+void printTree(Graph *graph, int rootIndex)
 {
 }
 
-void printNode(char *data)
+void printNode(Node *node)
 {
-    int width = strlen(data);
+    int width = strlen(node->key);
     bool isEven = width % 2 == 0;
     if (isEven)
         width++; // ensure odd
 
     // print top
     printf("╔");
-    repeatPrint('═', width);
+    repeatPrint('=', width);
     printf("╗\n");
 
     // print data
-    printf("║%s", data);
+    printf("║%s", node->key);
     if (isEven)
         printf(" "); // add filler if necessary
     printf("║\n");
 
     // print bottom
     printf("╚");
-    repeatPrint('═', width);
+    repeatPrint('=', width);
     printf("╝\n");
 }
 
